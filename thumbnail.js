a8bab4c3888db51f0f1fdeb41aa0ced99f5c819f
Skyspace0 <86726636+Skyspace0@users.noreply.github.com>
2021-07-03 14:47:51 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -3,14 +3,3 @@ document.addEventListener("DOMContentLoaded", function(event) {
 });
 let targetImage = document.querySelector("#smart-image");
 targetImage.classList.remove("small");
-        
-        
-        
-        
-        
-        
-        if (targetImage.classList.contains("small")) {
-targetImage.classList.remove("small")
-} else {
-targetImage.classList.add("small")
-}
