eb6328e58a42f737dd88467e5f106267d5d3488d
Skyspace0 <86726636+Skyspace0@users.noreply.github.com>
2021-07-03 14:23:47 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -7,7 +7,7 @@ targetImage.addEventListener("click", function() {
 });
 targetImage.classList.remove("small");
 if (targetImage.classList.contains("small")) {
-targetImage.classList.add("small")
-} else {
 targetImage.classList.remove("small")
+} else {
+targetImage.classList.add("small")
 }
