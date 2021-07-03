4067b7117e5dd6c2fef356ac482de7865dc28bb3
Skyspace0 <86726636+Skyspace0@users.noreply.github.com>
2021-07-03 14:38:03 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,7 +1,11 @@
-document.addEventListener("DOMContentLoaded")
-let targetImage = document.querySelector("#smart-image");
-targetImage.addEventListener("click", function() {
-targetImage.classList.remove("small");
+document.addEventListener("DOMContentLoaded", function(event) {
+    // 听好了,你的代码都必须写在下面!
+  //alert("Hello javascript");
+  let targetImage = document.querySelector("#smart-image");
+  targetImage.addEventListener
+  (
+    "click", function() 
+    { 
 if (targetImage.classList.contains("small")) {
 targetImage.classList.remove("small")
 } else {
